12265ada1e4b98e5e4023377e40efe56b1a028ce
Moussa Ball <moiseball20155@gmail.com>
2024-07-16 14:37:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
     res.status(401).send('Unauthorized: No token provided or token was invalid');
   }
 });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
