6d0c76e6e5d33669b150b5cec510226ab27a65f0
jmakwana01 <jaymakwanacodes@gmail.com>
2024-09-11 17:40:57 +0530

    update in dbconfig

--- src/api/config/dbConfig.js
+++ src/api/config/dbConfig.js
@@ -1,11 +1,18 @@
-const { Pool } = require('pg');
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'docker',
-  host: 'localhost',
-  database: 'zero_code_db',
-  password: 'docker',
-  port: 5433,
+  user:process.env.USER_DB,
+  host: process.env.HOST_DB,
+  database: process.env.DATABASE_DB,
+  password: process.env.PASSWORD_DB,
+  port: process.env.PORT_DB,
+  ssl: {
+    rejectUnauthorized: false  // This allows connecting without strict SSL verification
+}
 });
 
-module.exports = pool;
+export default pool;
